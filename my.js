71b8cd8d87abd537a6bce2240ac38e97c3c25a24
aarushibamal-cmyk <aarushibamal@gmail.com>
2026-07-04 21:03:53 +0530

    Revising Objects and CSS

--- my.js
+++ my.js
@@ -63,7 +63,7 @@ and for undefined it comes NAN and for boolean 0 for false and 1 for true */
 
 
 //***********************Operations*******************//
- 
+
 // let value = 56;
 // let negValue = - value;
 
@@ -192,7 +192,7 @@ ___________   Stack Memory
 
 //***********************Arrays***************************//
 
-const myArr = [1,2,3,"Aarushi"];
+// const myArr = [1,2,3,"Aarushi"];
 // console.log(myArr);
 // myArr[1] = myArr[3]
 // console.log(myArr);
@@ -238,4 +238,163 @@ B [ 1, 2, 3, 'Aarushi', 6 ]
 [ 1, 6 ]
 
 } */
+
+
+// let lotr = ["frodo" , "sam" , "aragon"]
+// const hp = ["harry" , "Ron" , "Ginny", "Hermione"]
+
+// lotr.push(hp);
+// console.log(lotr); => [ 'frodo', 'sam', 'aragon', [ 'harry', 'Ron', 'Ginny', 'Hermione' ] ]
+
+// lotr = lotr.concat(hp) => returns an array and does not modify the original array
+// console.log(lotr)
+// console.log(hp);
+
+// const newArr = [...lotr , ...hp]; => to concat two
+
+// console.log(newArr); => output => [
+//   'frodo',    'sam',
+//   'aragon',   'harry',
+//   'Ron',      'Ginny',
+//   'Hermione'
+// ]
+
+// const newArr = [1,2,3,[4,5,6],7,[6,8,[9,0]]]
+// const myReal = newArr.flat(Infinity)
+// console.log(myReal); => output => [
+//   1, 2, 3, 4, 5,
+//   6, 7, 6, 8, 9,
+//   0
+// ]
+// console.log(Array.isArray("Aarushi")) => false
+// console.log(Array.from("Aarushi")); => converts to array
+
+// console.log(Array.from({name: "aarushi"})); => []
+
+
+
+//**********************OBJECTS IN JS**********************//
+
+// singleton 
+
+// object literals
+// const mySym =  Symbol("key1")
+// const myuser = {
+//     name: "Aarushi",
+//     "full Name" : "Aarushi Bamal",
+//     [mySym] : "myKey1",
+//     class: 12,
+//     gmail: "aarushibamal@gmail.com",
+//     isGirl : true,
+//     lastLogin: ["monday","tuseday", "saturday"]
+// }
+// console.log(myuser.fullName); => undefined
+// console.log(myuser["full Name"]); => "Aarushi BAMAL"
+// console.log(typeof myuser.mySym); => string
+// console.log(myuser[mySym]);
+
+// myuser.name = "drinksre"
+// console.log(myuser.name);
+// Object.freeze(myuser) => does not let us change anything in the myuser object
+
+// console.log(0.1 + 0.2 === 0.3); => false as 0.1 + 0.2 = 0.300000000000000 which is not equal to 0.3
+// console.log(0.1 + 0.2);
+
+// myuser.greeting = function(){
+//    console.log("this is greeting");
+// }
+// console.log(myuser.greeting); => [Function (anonymous)]
+// console.log(myuser.greeting()); => this is greeting undefined
+
+// myuser.greeting2 = function name() {
+//     console.log(`Hello ${this["full Name"]}`); => or use this.name
+// }
+// console.log(myuser.greeting2);
+// console.log(myuser.greeting2());
+/* OUTPUT IS :-
+[Function: name]=> greeting2
+Hello Aarushi Bamal => for greeting2()
+undefined  */
+
+//***************SINGLETON******************/
+
+// const users =new Object()
+// console.log(users); => {}
+// users.id = "123abc"
+// users.name = "Arushi"
+// users.isLoggedIN = false
+// console.log(users);
+
+// const user2 = {
+//     email : "aarushi@min.com",
+//     fullName : {
+//         name: "Aarushi",
+//         lastName: "Bamel"
+//     }
+// }
+// console.log(user2.fullName.lastName);  
+
+// const obj1 = {
+//     1: "a",
+//     2: "b"
+// }
+// const obj2 = {
+//     3: "c",
+//     4: "d"
+// }
+// const obj3 = {obj1 , obj2}
+// console.log(obj3);  => { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+// const obj4 = Object.assign(obj1, obj2)
+// console.log(obj4); => { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+// const obj5 = Object.assign({},obj1, obj2) => same result
+
+// const obj5 = {...obj1 ,...obj2}
+/*converting an object to string 
+const obj = { name: "John", age: 30 };
+
+SON string
+const str = JSON.stringify(obj);
+console.log(str); // {"name":"John","age":30}
+
+Simple string representation
+console.log(String(obj)); // [object Object] */
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "aarush"
+//     },
+//     {
+//         id: 2,
+//         email: "awiehd"
+//     },
+//     {
+//         id: 3,
+//         email: "ejkeeh"
+//     }
+// ]
+// users[1].email
+// console.log(Object.keys(myuser)); => Output datatype is array all the keys are converted into an array of strings
+// console.log(Object.entries(myuser));
+/* OUTPUT IS 
+[
+  [ 'name', 'Aarushi' ],
+  [ 'full Name', 'Aarushi Bamal' ],
+  [ 'class', 12 ],
+  [ 'gmail', 'aarushibamal@gmail.com' ],
+  [ 'isGirl', true ],
+  [ 'lastLogin', [ 'monday', 'tuseday', 'saturday' ] ]
+] */
+
+// console.log(myuser.hasOwnProperty('name')); => true;
+// Destructiring of objects
+const course = {
+    courseName: "wer",
+    price: 9403,
+    courseInst: "AAarushi"
+}
+const {price} = course
+// console.log(price); => 9403
+const {price : p} = course
+console.log(p);
 
